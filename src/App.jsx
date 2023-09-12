abc54257d998315e4ff43da2c8d3e876c6aee3ec
Woojin Sim <ssqwe2@gmail.com>
2023-09-12 20:26:59 +0900

    여행경보기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,80 @@
 //import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import OpenAI from "openai";
+import axios from "axios";
 
 const openai = new OpenAI({
-  organization: `${process.env.OPENAI_APP_ORG}`,
-  apiKey: `${process.env.OPENAI_APP_API_KEY}`,
+  organization: `${process.env.REACT_APP_OPENAI_ORG}`,
+  apiKey: `${process.env.REACT_APP_OPENAI_API_KEY}`,
   dangerouslyAllowBrowser: true
 });
 
-const Resultdest = (props) => {
+
+const Resultable = (props) => {
+  const [alarmLvl, setAlarmLvl] = useState();
+  useEffect(() => {
+    const fetchSafeLvl = async () => {
+      if (props.iso == "none") {
+        setAlarmLvl("국가선택"); return;
+      }
+      var url = "http://apis.data.go.kr/1262000/TravelAlarmService2/getTravelAlarmList2";
+      var queryParams = "?" + encodeURIComponent("serviceKey") + "=" + process.env.REACT_APP_TRAVEL_SAFE_LVL_API_KEY;
+      queryParams += "&" + encodeURIComponent("returnType") + "=" + encodeURIComponent("JSON");
+      queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10");
+      queryParams += "&" + encodeURIComponent("cond[country_nm::EQ]") + "=" + encodeURIComponent(props.regionName);
+      queryParams += "&" + encodeURIComponent("cond[country_iso_alp2::EQ]") + "=" + encodeURIComponent(props.iso);
+      queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1");
+      console.log(url + queryParams)
+      try {
+        axios.get(url + queryParams).then((Response) => {
+          const rawData = Response.data.data;
+          if (rawData.length < 1) {setAlarmLvl("데이터를 가져올 수 없음"); return;}
+          
+          switch(rawData[0].alarm_lvl) {
+            case(1):
+              setAlarmLvl(<span className="safe-lvl-1">1단계 여행유의</span>); break;
+            case(2):
+              setAlarmLvl(<span className="safe-lvl-2">2단계 여행자제</span>); break;
+            case(3):
+              setAlarmLvl(<span className="safe-lvl-2">3단계 출국권고</span>); break;
+            case(4):
+              setAlarmLvl(<span className="safe-lvl-2">4단계 여행금지</span>); break;
+          }
+          
+        });
+      } catch {
+        console.log("에러")
+      }
+    }
+    fetchSafeLvl();
+  });
+
   return(
-    <span>현재 환율</span>
+    <table className="result-table">
+      <tbody>
+        <tr>
+          <td className="table-header">선택한 여행지</td>
+          <td>{props.regionName}({props.iso})</td>
+        </tr>
+        <tr>
+          <td className="table-header">여행위험도</td>
+          <td>{alarmLvl}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
 function App() {
-  const [selectedDest, setSelectedDest] = useState("none");
+  const [selectedDestinationInfo, setSelectedDestinationInfo] = useState(["none", null]); // 도착지정보 State [국가명, 국가ISO코드]
+  const [destName, setDestName] = useState();
+  //const [alarmLvl, setAlarmLvl] = useState();
   const destinationList = [
     { value: "none", name: "여행지 선택" },
     { value: "JP", name: "일본" },
-    { value: "US", name: "미국" },
-    { value: "CN", name: "중국" }
+    { value: "CN", name: "중국" },
+    { value: "VN", name: "베트남" }
   ];
 
   /*
@@ -45,8 +98,10 @@ function App() {
   */
 
   const changeDestination = async (e) => {
-    setSelectedDest(e.target.value)
-    console.log(e.target.value)
+    setSelectedDestinationInfo([
+      e.target.value,
+      e.target.options[e.target.selectedIndex].text
+    ]);
   }
 
   return (
@@ -54,12 +109,13 @@ function App() {
         <div className="rootBackground">
             <div className="outer-container">
               <div className="outer-title">여행가이드</div>
-              <select className="combo-box" id="destination-select" onChange={changeDestination} value={selectedDest}>
+              <select className="combo-box" id="destination-select" onChange={changeDestination} value={selectedDestinationInfo[0]}>
                 {destinationList.map((item) => (
                   <option value={item.value} key={item.name}>{item.name}</option>
                 ))}
               </select>
-              <Resultdest option={selectedDest}></Resultdest>
+
+              <Resultable iso={selectedDestinationInfo[0]} regionName={selectedDestinationInfo[1]}></Resultable>
               {/*
               <div className="inner-element">
                 <span className="element-title"></span>
